6a88c328a2a88b0221f137ad1bd0c689b3283a6c
GrouchyPanda <mckee.christopherj@gmail.com>
2017-11-18 16:50:17 -0800

    Making it work on Openshift

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 client.on("ready", () => {
   console.log("I am ready!");
