71a03de4bea0ccfd8a37aa2619e27b5d7f1fc260
GrouchyPanda <mckee.christopherj@gmail.com>
2017-11-18 22:39:21 -0800

    Adding some basic commands to test it out

--- src/app.js
+++ src/app.js
@@ -6,14 +6,29 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
 
 client.on("ready", () => {
   console.log("I am ready!");
+  client.user.setGame("doing robot things, beep boop");
 });
 
 client.on("message", (message) => {
-	if(!msg.content.startsWith(prefix) || message.author.bot) return;
+	if(!message.content.startsWith(config.prefix) || message.author.bot) return;
 	
-    if (message.content.startsWith(config.prefix + "ping")) {
-        message.channel.send("pong!");
-    }
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+	
+	switch (command) {
+		case "ping":
+			message.channel.send("pong!");
+			break;
+		case "hello":
+			message.send("Hello " + ${message.author});
+			break;
+		case "help":
+			message.send("Pshh, " + message.author + " out here needing help like a dummy...");
+			break;
+		default:
+			message.channel.send("Command not recognized. Please type !help to see my available commands.");
+			break;
+	}
 });
 
 client.login(config.token);
